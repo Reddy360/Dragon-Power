6bd0b575ce9db71684495fa0cc9ac4339093bcf1
Connor Graham <thecg1997@gmail.com>
2015-05-14 00:05:38 +0100

    Fixed landing bug and added main action

--- pe/nn/connor/dragonpower/dragons/EarthDragon.java
+++ pe/nn/connor/dragonpower/dragons/EarthDragon.java
@@ -1,10 +1,14 @@
 package pe.nn.connor.dragonpower.dragons;
 
+import java.util.HashSet;
+import java.util.Random;
+
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.FallingBlock;
 import org.bukkit.entity.Player;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -22,9 +26,35 @@ public class EarthDragon implements Dragon{
 		}
 	}
 
+	@SuppressWarnings("deprecation")
 	@Override
 	public void onClick(Player player, PlayerInteractEvent e) {
-		
+		if(player.isSneaking() && (e.getAction() == Action.LEFT_CLICK_AIR || e.getAction() == Action.LEFT_CLICK_BLOCK)){
+			HashSet<Material> set = new HashSet<Material>();
+			set.add(Material.AIR);
+			Block block = player.getTargetBlock(set, 100);
+			Location blockLocation = block.getLocation();
+			
+			int radius = 2;
+			for(int x = -radius; x <= radius; x++) {
+		        for(int y = -radius; y <= radius; y++) {
+		            for(int z = -radius; z <= radius; z++) {
+		            	Location location = new Location(player.getWorld(), x, y, z);
+		            	location.add(blockLocation);
+		            	Block targetBlock = location.getBlock();
+		            	if(targetBlock.getType() != Material.AIR){
+		    				Material material = targetBlock.getType();
+		    				byte data = targetBlock.getData();
+		    				targetBlock.setType(Material.AIR);
+		    				FallingBlock fallingBlock = player.getWorld().spawnFallingBlock(location, material, data);
+		    				//Make the falling block launch into the air
+		    				double random = new Random().nextDouble();
+		    				fallingBlock.setVelocity(new Vector(0, 1 + random, 0));
+		            	}
+		            }
+		        }
+		    }
+		}
 	}
 
 	@SuppressWarnings("deprecation")
@@ -34,10 +64,10 @@ public class EarthDragon implements Dragon{
 			//Falling = earth force effect + no damage
 			//Get block under exactly
 			Location blockUnder = player.getLocation().getBlock().getLocation().subtract(0, 1, 0);
-			Location front = blockUnder.add(1, 0, 0);
-			Location back = blockUnder.subtract(1, 0, 0);
-			Location left = blockUnder.add(0, 0, 1);
-			Location right = blockUnder.subtract(0, 0, 1);
+			Location front = blockUnder.clone().add(1, 0, 0);
+			Location back = blockUnder.clone().subtract(1, 0, 0);
+			Location left = blockUnder.clone().add(0, 0, 1);
+			Location right = blockUnder.clone().subtract(0, 0, 1);
 			Location[] locations = {
 				front,
 				back,
